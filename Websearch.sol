20ebbcaefa3413e0b0ce0804b8e48bdb78a28e64
Leon Do <leondo123@gmail.com>
2024-05-09 11:11:01 -0400

    Create Websearch.sol

---
+++ Websearch.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "https://github.com/galadriel-ai/contracts/blob/main/contracts/contracts/interfaces/IOracle.sol";
+
+contract Websearch {
+    address private oracleAddress = 0x4168668812C94a3167FCd41D12014c5498D74d7e;
+    mapping (address => string) public responses;
+
+    constructor() {}
+
+    function promptWeb(string memory _message) public returns (uint256) {
+        uint256 runId =  uint256(uint160(msg.sender));
+        IOracle(oracleAddress).createFunctionCall(
+            runId,
+            "web_search",
+            _message
+        );
+        return runId;
+    }
+
+    function onOracleFunctionResponse(
+        uint _runId,
+        string memory _response,
+        string memory _errorMessage
+    ) public {
+        require(msg.sender == oracleAddress, "Caller is not oracle");
+        address toAddress = address(uint160(uint256(_runId)));
+        if (keccak256(abi.encodePacked(_errorMessage)) != keccak256(abi.encodePacked(""))) {
+            responses[toAddress] = _errorMessage;
+        } else {
+            responses[toAddress] = _response;
+        }
+    }
+}
